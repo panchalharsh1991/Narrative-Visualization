c7e99b5bd125d505e506ae8050ab6c3b720f0eb5
panchalharsh1991 <panchalharsh1991@gmail.com>
2019-07-21 21:12:47 -0500

    load csv data

---
+++ js/loadcsvdata.js
@@ -0,0 +1,22 @@
+function loadcsvdata( dataloaded ) {
+    d3.dsv(",", "./crime.csv", function(d) {
+
+        const dataobj = {
+			ucr_part: d.UCR_PART,
+			year: +d.YEAR,
+            month: d.MONTH_NAME,
+			day: d.DAY_OF_WEEK,
+			hour: +d.HOUR,
+			date: d.OCCURRED_ON_DATE,
+            offense: d.OFFENSE_CODE_GROUP,
+			desc: d.OFFENSE_DESCRIPTION
+			street: d.STREET
+        };
+
+        return dataobj;
+
+    }).then(function(data) {
+        dataSet = data;
+        dataloaded();
+    });
+}
