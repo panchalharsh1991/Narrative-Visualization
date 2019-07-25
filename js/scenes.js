60929620869daab406f26c015da01cd4d50f0c18
panchalharsh1991 <panchalharsh1991@gmail.com>
2019-07-25 18:37:13 -0500

    sorting data

--- js/scenes.js
+++ js/scenes.js
@@ -48,6 +48,9 @@ const yAxis4 = d3.axisLeft();
 
 function calculateScales() {
     const referenceData = d3.values(offenseGroups);
+	referenceData.sort(function(x, y){
+						return d3.ascending(x.offenseCount, y.offenseCount);
+						});
 	console.log(referenceData);
 	const referenceData2 = d3.values(offensesByDay);
 	console.log(referenceData2);
@@ -57,7 +60,8 @@ function calculateScales() {
 	console.log(referenceData4);
 	
 	x_offenses.range([0, chart_dimensions.width])
-        .domain(d3.keys(offenseGroups));
+        .domain(d3.keys(referenceData));
+		//.domain(d3.keys(offenseGroups));
     y_offenseCount.domain([0, d3.max(referenceData, function(d) { return d.offenseCount; })])
         .range([0, chart_dimensions.height]);
 	y_offenseCount_axis.domain([0, d3.max(referenceData, function(d) { return d.offenseCount; })])
@@ -97,7 +101,8 @@ var div = d3.select("body").append("div");
 	
     d3.select(".chart")
 		.selectAll(".bar-papers-group")
-        .data(d3.values(offenseGroups))
+        .data(referenceData)
+		//.data(d3.values(offenseGroups))
         .enter()
         .append("g")
         .classed("bar-papers-group",true)
@@ -183,7 +188,8 @@ function showOffenseCountAxis() {
 
 function showOffenseAxis() {
     const xAxis = d3.axisBottom().scale(x_offenses)
-        .ticks(d3.keys(offenseGroups));
+        .ticks(d3.keys(referenceData));
+		//.ticks(d3.keys(offenseGroups));
 
     d3.select(".chart").append("g")
         .attr("id", "xAxisG")
