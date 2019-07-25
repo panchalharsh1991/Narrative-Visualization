057bb4690d7c19ae2e099259421fa07f56b49568
panchalharsh1991 <panchalharsh1991@gmail.com>
2019-07-25 17:40:52 -0500

    sorting data

--- js/scenes.js
+++ js/scenes.js
@@ -20,10 +20,10 @@ var animateFunctions = [
     [animateScene4,deanimateScene4]
 ];
 
-var offenseGroups = {};
+/*var offenseGroups = {};
 var offensesByDay = {};
 var offensesByHour = {};
-var offensesByMonth = {};
+var offensesByMonth = {};*/
 
 const x_offenses = d3.scaleBand();
 const y_offenseCount = d3.scaleLinear();
