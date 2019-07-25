745a7f578f6a468c309e3d2487ed55af47d5e587
panchalharsh1991 <panchalharsh1991@gmail.com>
2019-07-24 23:05:57 -0500

    scenes2 change

--- js/scenes.js
+++ js/scenes.js
@@ -25,21 +25,36 @@ const offensesByDay = {};
 const offensesByHour = {};
 let chart;
 
-var x_offenses = d3.scaleBand();
-var y_offenseCount = d3.scaleLinear();
-var y_offenseCount_axis = d3.scaleLinear();
-var yAxis = d3.axisLeft();
-var xAxis = d3.axisBottom();
+const x_offenses = d3.scaleBand();
+const y_offenseCount = d3.scaleLinear();
+const y_offenseCount_axis = d3.scaleLinear();
+const yAxis = d3.axisLeft();
 
-function calculateScales(data) {
-    var referenceData = d3.values(data);
+const x_days = d3.scaleBand();
+const y_offensesByDayCount = d3.scaleLinear();
+const y_offensesByDayCount_axis = d3.scaleLinear();
+const yAxis2 = d3.axisLeft();
+
+
+function calculateScales() {
+    const referenceData = d3.values(offenseGroups);
 	console.log(referenceData);
+	const referenceData2 = d3.values(offensesByDay)
+	console.log(referenceData2);
+	//console.log(d3.values(offensesByHour));
 	
 	x_offenses.range([0, chart_dimensions.width])
-        .domain(d3.keys(data));
+        .domain(d3.keys(offenseGroups));
     y_offenseCount.domain([0, d3.max(referenceData, function(d) { return d.offenseCount; })])
         .range([0, chart_dimensions.height]);
 	y_offenseCount_axis.domain([0, d3.max(referenceData, function(d) { return d.offenseCount; })])
+        .range([chart_dimensions.height, 0]);
+		
+	x_days.range([0, chart_dimensions.width])
+        .domain(d3.keys(offensesByDay));
+    y_offensesByDayCount.domain([0, d3.max(referenceData2, function(d) { return d.offenseCount; })])
+        .range([0, chart_dimensions.height]);
+	y_offensesByDayCount_axis.domain([0, d3.max(referenceData2, function(d) { return d.offenseCount; })])
         .range([chart_dimensions.height, 0]);	
 }
 
@@ -48,13 +63,12 @@ function initializeChartArea() {
         .attr("width", canvas.width)
         .attr("height", canvas.height);
 }
-
-function createOffenseCountBars(data) {
+function createOffenseCountBars() {
 var div = d3.select("body").append("div");
 	
     d3.select(".chart")
 		.selectAll(".bar-papers-group")
-        .data(d3.values(data))
+        .data(d3.values(offenseGroups))
         .enter()
         .append("g")
         .classed("bar-papers-group",true)
@@ -138,9 +152,9 @@ function showOffenseCountAxis() {
             ", rotate(-90)");
 }
 
-function showOffenseAxis(data,desc) {
-    xAxis.scale(x_offenses)
-        .ticks(d3.keys(data));
+function showOffenseAxis() {
+    const xAxis = d3.axisBottom().scale(x_offenses)
+        .ticks(d3.keys(offenseGroups));
 
     d3.select(".chart").append("g")
         .attr("id", "xAxisG")
@@ -161,7 +175,7 @@ function showOffenseAxis(data,desc) {
             "translate(" + (margin.left + chart_dimensions.width / 2) + " ," +
             (margin.top + chart_dimensions.height + 50) + ")")
         .style("text-anchor", "middle")
-        .text(desc);
+        .text("Offense Group");
 }
 
 function wrap(text, width) {
@@ -188,6 +202,121 @@ function wrap(text, width) {
   })
 }
 
+function createOffensesByDayCountBars() {
+var div = d3.select("body").append("div");
+	
+    d3.select(".chart")
+		.selectAll(".bar-papers-group")
+        .data(d3.values(offensesByDay))
+        .enter()
+        .append("g")
+        .classed("bar-papers-group",true)
+        .attr("transform",
+            function (d) {
+                return "translate(" + (margin.left + (20 + x_days(d.day)-x_days.bandwidth()/2)) + ", " + margin.top + ")";
+            })
+        .append("rect")
+        .classed("bar-papers-rect",true)
+        .attr("x", x_days.bandwidth()/2)
+        .attr("y", chart_dimensions.height)
+		.attr("width", x_days.bandwidth()/2 - 1)
+        .attr("height",0)
+		.on("mouseover", function (d) {
+			//console.log(d3.event.pageX + ":" + d3.event.pageY);
+			div.transition()
+            .duration(200)
+            .style("opacity", .9);
+			div.html(d.offenseCount)
+			.style("position","absolute")
+            .style("left", (d3.event.pageX) + "px")
+            .style("top", (d3.event.pageY - 28) + "px");
+     })
+		.on("mouseout", function (d) {
+			div.transition()
+            .duration(1000)
+            .style("opacity", 0);
+     });
+}
+
+function showOffensesByDayCountBars() {
+
+    d3.selectAll(".bar-papers-rect")
+        .transition()
+        .duration(1000)
+        .attr("height", function (d) {
+            return y_offensesByDayCount(d.offenseCount);
+        })
+        .attr("y", function (d) {
+            return (chart_dimensions.height - y_offensesByDayCount(d.offenseCount));
+        });
+}
+
+function createOffensesByDayCountAxis() {
+    yAxis2.scale(y_offensesByDayCount_axis)
+        .tickSize(10).ticks(20);
+
+    d3.select(".chart").append("g")
+        .attr("id", "yAxisPapersG")
+        .classed("y-axis-papers",true)
+        .attr("transform", "translate(" + margin.left + "," + (margin.top + chart_dimensions.height + margin.bottom) + ")")
+        .call(yAxis2);
+
+    d3.select("svg").append("text")
+        .attr("id", "yAxisPapersLabel")
+        .attr("transform",
+            "translate(8," + (margin.top + chart_dimensions.height + margin.bottom + chart_dimensions.height / 2) + ")" +
+            ", rotate(-90)")
+        .style("text-anchor", "middle")
+        .text("Number of Records");
+}
+
+function showOffensesByDayCountAxis() {
+    d3.select("#yAxisPapersG")
+        .transition()
+        .duration(1000)
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+        .call(yAxis2)
+        .selectAll("text")
+        .attr("x", -50)
+        .attr("y", 0)
+        .attr("dx", 0)
+        .attr("dy", "0.35em")
+        .style("text-anchor", "start");
+
+    d3.select("#yAxisPapersLabel")
+        .transition()
+        .duration(1000)
+        .attr("transform",
+            "translate(8," + (margin.top + chart_dimensions.height / 2) + ")" +
+            ", rotate(-90)");
+}
+
+function showDaysAxis() {
+    const xAxis = d3.axisBottom().scale(x_days)
+        .ticks(d3.keys(offensesByDay));
+
+    d3.select(".chart").append("g")
+        .attr("id", "xAxisG")
+        .classed("x axis",true)
+        .attr("transform", "translate(" + margin.left + "," + (margin.top + chart_dimensions.height) + ")")
+        .call(xAxis)
+        .selectAll("text")
+		.call(wrap, x_days.bandwidth())
+        .attr("x", -20)
+        .attr("y", 20)
+        .attr("dx", 0)
+        .attr("dy", "0.35em")
+        .attr("transform", "rotate(0)")
+        .style("text-anchor", "start");
+
+    d3.select(".chart").append("text")
+        .attr("transform",
+            "translate(" + (margin.left + chart_dimensions.width / 2) + " ," +
+            (margin.top + chart_dimensions.height + 50) + ")")
+        .style("text-anchor", "middle")
+        .text("Days");
+}
+
 function animateScene( forward ) {
     if (frame > (animateFunctions.length-1)) return;
 
@@ -198,24 +327,24 @@ function animateScene( forward ) {
 
 function animateScene0() {
     initializeChartArea();
-    calculateScales(offenseGroups);
+    calculateScales();
 
-    createOffenseCountBars(offenseGroups);
+    createOffenseCountBars();
 	showOffenseCountBars();
 	createOffenseCountAxis();
 	showOffenseCountAxis();
-	showOffenseAxis(offenseGroups,"Offense Group");
+	showOffenseAxis();
 }
 
 function animateScene1() {
 	initializeChartArea();
-    calculateScales(offensesByDay);
+    calculateScales();
 
-    createOffenseCountBars(offensesByDay);
-	showOffenseCountBars();
-	createOffenseCountAxis();
-	showOffenseCountAxis();
-	showOffenseAxis(offensesByDay,"Days of Week");
+    createOffensesByDayCountBars();
+	showOffensesByDayCountBars();
+	createOffensesByDayCountAxis();
+	showOffensesByDayCountAxis();
+	showDaysAxis();
 }
 
 function animateScene2() {
