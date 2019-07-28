e3ab02bdfc1d8c02ffe63c8207ef960f2ec4bd91
panchalharsh1991 <panchalharsh1991@gmail.com>
2019-07-28 13:40:59 -0500

    scene desc changes

--- js/scenes.js
+++ js/scenes.js
@@ -669,14 +669,14 @@ function chart(csv) {
 	d3.select(".heading").insert("br");
 	d3.select(".heading").insert("br");
 	d3.select(".heading").insert("h4").text("Explore Yourself").style("text-anchor", "start");
-	d3.select("#chart-div").insert("div").classed("parascenes",true).style('width','300px').style('height','180px');
+	d3.select(".heading").insert("div").classed("parascenes",true).style('width','300px').style('height','180px');
 	d3.select(".parascenes").insert("p").text("Select type of a crime to see its frequency over the hours of a day."); 
 	d3.select(".heading").insert("br");
 	d3.select(".parascenes").insert("p").text("You can select Sorted Data checkbox to sort the data in descending order of frequency.");
 	
 	d3.select(".parascenes").insert("div").classed("selection",true);
 	d3.select(".selection").insert("br");
-	d3.select(".selection").insert("h4").text("Select Offense:");
+	d3.select(".selection").insert("h2").text("Select Offense:");
 	d3.select(".selection").insert("select").classed("offense",true);
 	d3.select(".selection").insert("br");
 	d3.select(".selection").insert("br");
